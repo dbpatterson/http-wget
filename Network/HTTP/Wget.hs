9b477834f42866aef23d4323d504a04da688d554
daniel <daniel@kry.localdomain>
2010-01-05 23:52:38 -0500

    modified to get headers and present a wget' function that returns headers and body.

--- Network/HTTP/Wget.hs
+++ Network/HTTP/Wget.hs
@@ -15,6 +15,7 @@
 ---------------------------------------------------------
 module Network.HTTP.Wget
     ( wget
+    , wget'
     , WgetException (..)
     ) where
 
@@ -23,10 +24,13 @@ import System.Exit
 import System.IO
 import Numeric (showHex)
 import Data.List (intercalate)
+import Data.List.Split (splitOn)
 import Control.Monad.Trans
 import Control.Failure
 import Control.Exception
 import Data.Generics
+import Data.Char (isSpace)
+import Data.Text (strip)
 
 newtype WgetException = WgetException String
     deriving (Show, Typeable)
@@ -38,7 +42,15 @@ wget :: (MonadIO m, MonadFailure WgetException m)
      -> [(String, String)] -- ^ Get parameters.
      -> [(String, String)] -- ^ Post parameters. If empty, this will be a get request.
      -> m String -- ^ The response body.
-wget url get post = do
+wget url get post = wget' url get post >>= return.snd
+
+-- | Get a response from the given URL with the given parameters, including headers.
+wget' :: (MonadIO m, MonadFailure WgetException m)
+     => String -- ^ The URL.
+     -> [(String, String)] -- ^ Get parameters.
+     -> [(String, String)] -- ^ Post parameters. If empty, this will be a get request.
+     -> m ([(String, String)], String) -- ^ The headers and response body.
+wget' url get post = do
     let getSepChar :: Char
         getSepChar = if '?' `elem` url then '&' else '?'
         get' :: String
@@ -49,12 +61,23 @@ wget url get post = do
                     else ["--post-data", urlEncodePairs post]
     (Nothing, Just hout, Just herr, phandle) <- liftIO $
         createProcess $ (proc "wget"
-            ((url ++ get') : post' ++ ["-O", "-"])
+            ((url ++ get') : post' ++ ["-O", "-", "--save-headers"])
         ) { std_out = CreatePipe, std_err = CreatePipe }
     exitCode <- liftIO $ waitForProcess phandle
     case exitCode of
-        ExitSuccess -> liftIO $ hGetContents hout
+        ExitSuccess -> liftIO $ hGetContents hout >>= return . parseHeaders
         _ -> liftIO (hGetContents herr) >>= failure . WgetException
+
+parseHeaders :: String -> ([(String, String)], String)
+parseHeaders resp = parseHeaders' (drop 1 (lines resp)) []
+
+parseHeaders' :: [String] -> [(String, String)] -> ([(String, String)], String)
+parseHeaders' (s:ss) hs = if all isSpace s
+                            then (reverse hs, unlines ss) 
+                            -- not sure if http spec allows header values to have colons in them, but incase yes.
+                            else parseHeaders' ss ((head lt, (reverse.(dropWhile isSpace).reverse) (intercalate ": " (tail lt))):hs)
+    where lt = splitOn ": " s
+parseHeaders' [] hs = error "parseHeaders' failure: empty string list"
 
 urlEncodePairs :: [(String, String)] -> String
 urlEncodePairs = intercalate "&" . map urlEncodePair
